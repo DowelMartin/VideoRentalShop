f65ca84515ca1d19c70999d93d54b20068954f40
Irek <irek.przybylo@gmail.com>
2021-06-17 16:12:14 +0200

    VideoTape Delete VIew

--- videorentalshop/VideoRental/urls.py
+++ videorentalshop/VideoRental/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from VideoRental.views import (
-    VideoTapeListView, VideoTapeDetailView, VideoTapeCreateView, VideoTapeUpdateView)
+    VideoTapeListView, VideoTapeDetailView, VideoTapeCreateView, VideoTapeUpdateView, VideoTapeDeleteView)
 
 
 app_name = "videotapes"
 urlpatterns = [path("", VideoTapeListView.as_view(), name="list"),
                path("create/", VideoTapeCreateView.as_view(), name="create"),
                path("<str:slug>/", VideoTapeDetailView.as_view(), name="detail"),
-               path("<str:slug>/update/", VideoTapeUpdateView.as_view(), name="update"),]
+               path("<str:slug>/update/", VideoTapeUpdateView.as_view(), name="update"),
+               path("<str:slug>/delete/", VideoTapeDeleteView.as_view(), name="delete")]
